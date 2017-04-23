6f02d0785db2f8ceebad1b84ab788b54dd7094b0
Valentine Bichkovsky <valentin@sammy.spinbollaan.nl>
2017-04-24 00:50:19 +0200

    Finished FastAnagrams

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -10,17 +10,15 @@ import qualified Data.Set as S
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-fastAnagrams ::
-  Chars
-  -> Filename
-  -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams :: Chars -> Filename -> IO (List Chars)
+fastAnagrams word file = let combos = permutations word
+                             swords = S.fromList (hlist combos)
+                             toDict = S.fromAscList . hlist . lines
+                             toOut dict = listh . S.toList $
+                               S.intersection swords (toDict dict)
+                         in toOut <$> readFile file
 
-newtype NoCaseString =
-  NoCaseString {
-    ncString :: Chars
-  }
+newtype NoCaseString = NoCaseString {ncString :: Chars}
 
 instance Eq NoCaseString where
   (==) = (==) `on` map toLower . ncString
