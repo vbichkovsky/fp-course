59a70d6c7c1a08dc3056357d620ecf2d5a92f0fe
Valentine Bichkovsky <valentin@sammy.spinbollaan.nl>
2017-04-23 19:12:50 +0200

    Finished Anagrams

--- src/Course/Anagrams.hs
+++ src/Course/Anagrams.hs
@@ -28,17 +28,10 @@ Functions that might help
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-anagrams ::
-  Chars
-  -> Filename
-  -> IO (List Chars)
-anagrams =
-  error "todo: Course.Anagrams#anagrams"
+anagrams :: Chars -> Filename -> IO (List Chars)
+anagrams w file = findMatching (permutations w) <$> lines <$> readFile file
+  where findMatching = intersectBy equalIgnoringCase
 
 -- Compare two strings for equality, ignoring case
-equalIgnoringCase ::
-  Chars
-  -> Chars
-  -> Bool
-equalIgnoringCase =
-  error "todo: Course.Anagrams#equalIgnoringCase"
+equalIgnoringCase :: Chars -> Chars -> Bool
+equalIgnoringCase = (==) `on` map(toLower)
